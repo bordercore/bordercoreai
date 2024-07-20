69c6445d4e0d55ea00299d2590134471d122c020
F. Jerrell Schivers <jerrell@bordercore.com>
2024-07-20 17:11:45 -0400

    Add entry to .gitignore for local files

--- supervisord-script.sh
+++
@@ -1,7 +0,0 @@
-if [ -z "$1" ]; then
-    echo "Error: No chatbot argument supplied. Must be 'chatgpt' or 'floyd'" >&2
-    exit 1
-fi
-
-source /home/jerrell/.bashrc-bordercore
-/home/jerrell/dev/envs/chatbot/bin/python3 /home/jerrell/dev/chatbot/chatbot.py -m $1
