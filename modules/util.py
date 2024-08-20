bb0e75f3fc180e22b6a5982a6a155b96a13210bd
F. Jerrell Schivers <jerrell@bordercore.com>
2024-08-20 17:01:21 -0400

    Fix Gemma2 inference bug
    Only call tokenizer.add_special_tokens() for this model

--- modules/util.py
+++ modules/util.py
@@ -12,13 +12,9 @@ def get_tokenizer(model_path):
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
 
-    # model.resize_token_embeddings(len(tokenizer))
-
-    # Add tokens <|im_start|> and <|im_end|>; the latter is special eos token
-    # self.tokenizer.add_tokens(["<|im_start|>"])
-
     # Required for the unsloth_gemma-2-2b-it-bnb-4bit model
-    tokenizer.add_special_tokens(dict(eos_token="<end_of_turn>"))
+    if "unsloth_gemma-2-2b-it-bnb-4bit" in model_path:
+        tokenizer.add_special_tokens(dict(eos_token="<end_of_turn>"))
 
     return tokenizer
 
