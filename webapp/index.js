7e2281bfa90fa2550369b52e6a09fe54bb22b71b
F. Jerrell Schivers <jerrell@bordercore.com>
2025-04-15 15:52:50 -0400

    URL encode text for TTS

--- webapp/index.js
+++ webapp/index.js
@@ -696,7 +696,7 @@ const app = createApp({
 
             const voice = session.tts_voice;
             const outputFile = "stream_output.wav";
-            const streamingUrl = `http://${ttsHost.value}/?text=${response}&voice=${voice}&language=en&output_file=${outputFile}`;
+            const streamingUrl = `http://${ttsHost.value}/?text=${encodeURIComponent(response)}&voice=${voice}&language=en&output_file=${outputFile}`;
             audioElement.src = streamingUrl;
             audioMotion.gradient = "steelblue";
             audioMotion.volume = 1;
